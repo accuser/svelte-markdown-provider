09c95e555a2f10320eca4c881233001d2daf3ef8
Matthew Gibbons <mhgibbons@me.com>
2024-08-28 10:40:14 +0000

    Export defaults

---
+++ src/lib/types/directives.ts
@@ -0,0 +1,13 @@
+export type Directives<T extends import('mdast').Node = import('mdast-util-directive').Directives> =
+	{
+		[K in T['type']]: Record<
+			string,
+			import('svelte').Component<Extract<T, { type: K }>> | undefined
+		>;
+	};
+
+export const defaultDirectives: Directives = {
+	containerDirective: {},
+	leafDirective: {},
+	textDirective: {}
+};
