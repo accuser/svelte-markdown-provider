c17100097ad633dc402201a562a17ba14aa7e8be
Matthew Gibbons <mhgibbons@me.com>
2024-08-28 10:38:57 +0000

    Configured vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { svelteTesting } from '@testing-library/svelte/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), svelteTesting()],
 	test: {
 		environment: 'jsdom',
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		setupFiles: ['./vitest-setup.js']
 	}
 });
