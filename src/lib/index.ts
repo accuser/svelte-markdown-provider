8e81bf6d1975e977aa3c7dcb01fbc0d247fe1757
Matthew Gibbons <mhgibbons@me.com>
2024-08-28 10:39:55 +0000

    Export types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,4 @@
 export { default as Markdown } from './components/Markdown.svelte';
 export { default as MarkdownProvider } from './providers/MarkdownProvider.svelte';
+export type { Components } from './types/components.js';
+export type { Directives } from './types/directives.js';
