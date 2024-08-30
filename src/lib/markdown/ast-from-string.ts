f72ef56d8f7ebfb28c872556c68989687079097e
Matthew Gibbons <mhgibbons@me.com>
2024-08-30 08:50:04 +0000

    Provision for default extensions

--- src/lib/markdown/ast-from-string.ts
+++ src/lib/markdown/ast-from-string.ts
@@ -4,8 +4,13 @@ import { gfmFromMarkdown } from 'mdast-util-gfm';
 import { frontmatter } from 'micromark-extension-frontmatter';
 import { gfm } from 'micromark-extension-gfm';
 
-export default (src: string) =>
-	fromMarkdown(src, {
-		extensions: [frontmatter(['yaml']), gfm()],
-		mdastExtensions: [frontmatterFromMarkdown(['yaml']), gfmFromMarkdown()]
-	});
+export const DEFAULT_EXTENSIONS = [frontmatter(['yaml']), gfm()];
+export const DEFAULT_MDAST_EXTENSIONS = [frontmatterFromMarkdown(['yaml']), gfmFromMarkdown()];
+
+export default ((
+	src: string,
+	{ extensions = DEFAULT_EXTENSIONS, mdastExtensions = DEFAULT_MDAST_EXTENSIONS } = {}
+) => fromMarkdown(src, { extensions, mdastExtensions })) as (
+	src: string,
+	options?: import('mdast-util-from-markdown').Options
+) => import('mdast').Root;
