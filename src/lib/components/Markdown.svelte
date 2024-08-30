0f3052bb9937beaadf7c348461bf013160436b36
Matthew Gibbons <mhgibbons@me.com>
2024-08-30 05:34:13 +0000

    Don't spread defaults

--- src/lib/components/Markdown.svelte
+++ src/lib/components/Markdown.svelte
@@ -1,44 +1,37 @@
 <script lang="ts">
 	import { MARKDOWN_COMPONENTS_TOKEN } from '$lib/tokens/markdown-components.token.js';
 	import { MARKDOWN_DIRECTIVES_TOKEN } from '$lib/tokens/markdown-directives.token.js';
-	import { type Components, defaultComponents } from '$lib/types/components.js';
-	import { type Directives, defaultDirectives } from '$lib/types/directives.js';
+	import { defaultComponents, type Components } from '$lib/types/components.js';
+	import { defaultDirectives, type Directives } from '$lib/types/directives.js';
 	import { getContext, hasContext, setContext } from 'svelte';
 
 	const {
-		components: _components,
-		directives: _directives,
+		components,
+		directives,
 		node
 	}: { components?: Components; directives?: Directives; node: import('mdast').Nodes } = $props();
 
-	const components = {
-		...defaultComponents,
-		...(_components
-			? setContext(MARKDOWN_COMPONENTS_TOKEN, _components)
-			: hasContext(MARKDOWN_COMPONENTS_TOKEN)
-				? getContext<Components>(MARKDOWN_COMPONENTS_TOKEN)
-				: {})
-	};
+	const resolvedComponents = components
+		? setContext(MARKDOWN_COMPONENTS_TOKEN, components)
+		: hasContext(MARKDOWN_COMPONENTS_TOKEN)
+			? getContext<Components>(MARKDOWN_COMPONENTS_TOKEN)
+			: ({} as Components);
 
-	const directives = {
-		...defaultDirectives,
-		...(_directives
-			? setContext(MARKDOWN_DIRECTIVES_TOKEN, _directives)
-			: hasContext(MARKDOWN_DIRECTIVES_TOKEN)
-				? getContext<Directives>(MARKDOWN_DIRECTIVES_TOKEN)
-				: {})
-	};
-
-	const { type } = node;
+	const resolvedDirectives = directives
+		? setContext(MARKDOWN_DIRECTIVES_TOKEN, directives)
+		: hasContext(MARKDOWN_DIRECTIVES_TOKEN)
+			? getContext<Directives>(MARKDOWN_DIRECTIVES_TOKEN)
+			: ({} as Directives);
 
 	const Component =
-		directives &&
-		(type === 'containerDirective' || type === 'leafDirective' || type === 'textDirective')
-			? directives[type][node.name]
-			: components[type];
+		node.type === 'containerDirective' ||
+		node.type === 'leafDirective' ||
+		node.type === 'textDirective'
+			? (resolvedDirectives[node.type][node.name] ?? defaultDirectives[node.type][node.name])
+			: (resolvedComponents[node.type] ?? defaultComponents[node.type]);
 
 	if (!Component) {
-		console.warn(`Unrecognized node type "${type}"`);
+		console.warn(`Unrecognized node type "${node.type}"`);
 	}
 </script>
 
