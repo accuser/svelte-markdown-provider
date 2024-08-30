f97ae0a4a7a90dbbb77329bc0d23e1aef17e0456
Matthew Gibbons <mhgibbons@me.com>
2024-08-30 08:39:14 +0000

    Better example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,32 @@
 <script lang="ts">
 	import Markdown from '$lib/components/Markdown.svelte';
+	import astFromString from '$lib/markdown/ast-from-string.js';
+	import { parse } from 'yaml';
 	import type { PageData } from './$types.js';
 
 	const { data }: { data: PageData } = $props();
 
-	const { root } = $derived(data);
+	const { ast, frontmatter } = $derived.by(() => {
+		const ast = astFromString(data.src);
+
+		const frontmatter =
+			ast.type === 'root' && ast.children && ast.children[0].type === 'yaml'
+				? parse((ast.children.shift() as import('mdast').Yaml).value)
+				: undefined;
+
+		return { ast, frontmatter };
+	});
+
+	const { title } = $derived(frontmatter);
 </script>
 
 <article class="prose prose-lg max-w-prose mx-auto prose-slate dark:prose-invert">
-	<Markdown node={root} />
+	{#if frontmatter}
+		<header>
+			<h1>{title}</h1>
+		</header>
+	{/if}
+	<main>
+		<Markdown {ast} />
+	</main>
 </article>
