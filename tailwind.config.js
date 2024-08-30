b10d62de8b4301abd185bb452156b3695b282352
Matthew Gibbons <mhgibbons@me.com>
2024-08-30 09:54:39 +0000

    Example styling of inline code

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,31 @@ import typography from '@tailwindcss/typography';
 export default {
     content: ['src/**/*.svelte'],
     theme: {
-        extend: {},
+        extend: {
+            typography(theme) {
+                return {
+                    DEFAULT: {
+                        css: {
+                            'code::before': {
+                                content: 'none',
+                            },
+                            'code::after': {
+                                content: 'none',
+                            },
+                            code: {
+                                color: 'var(--tw-prose-pre-bg)',
+                                backgroundColor: 'var(--tw-prose-pre-code)',
+                                borderRadius: theme('borderRadius.DEFAULT'),
+                                paddingLeft: theme('spacing.1'),
+                                paddingRight: theme('spacing.1'),
+                                paddingTop: theme('spacing[0.5]'),
+                                paddingBottom: theme('spacing[0.5]'),
+                            },
+                        },
+                    }
+                }
+            }
+        },
     },
     plugins: [typography()],
 }
