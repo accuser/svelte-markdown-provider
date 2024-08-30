e2eac1747242741acc960c8924ad99f891c7b93f
Matthew Gibbons <mhgibbons@me.com>
2024-08-30 08:37:21 +0000

    Not required

--- src/lib/providers/MarkdownProvider.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import { defaultComponents } from '$lib/types/components.js';
-	import { defaultDirectives } from '$lib/types/directives.js';
-</script>
-
-<slot components={defaultComponents} directive={defaultDirectives} />
