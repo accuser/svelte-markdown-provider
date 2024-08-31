20f1231d7e6afc4b0e8422b4a8a1c625c633a8cc
Matthew Gibbons <mhgibbons@me.com>
2024-08-31 06:11:57 +0000

    Render math with katex

--- src/lib/components/InlineMath.svelte
+++ src/lib/components/InlineMath.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+	import katex from 'katex';
+
 	const { node }: { node: import('mdast-util-math').InlineMath } = $props();
 
 	const { data, value } = $derived(node);
+
+	const html = $derived.by(() =>
+		katex.renderToString(value, {
+			throwOnError: false,
+			displayMode: false,
+			output: 'html'
+		})
+	);
 </script>
 
-<code class="language-math math-inline">{value}</code>
+<code class="language-math math-inline">{@html html}</code>
--- src/lib/components/Math.svelte
+++ src/lib/components/Math.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+	import katex from 'katex';
+
 	const { node }: { node: import('mdast-util-math').Math } = $props();
 
 	const { data, meta, value } = $derived(node);
+
+	const html = $derived.by(() =>
+		katex.renderToString(value, {
+			throwOnError: false,
+			displayMode: true,
+			output: 'html'
+		})
+	);
 </script>
 
-<pre><code class="language-math math-display" data-meta={meta}>{value}</code></pre>
+<pre><code class="language-math math-display" data-meta={meta}>{@html html}</code></pre>
