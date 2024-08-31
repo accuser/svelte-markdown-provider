08cdd926db3e34e4c7147a540f99e8f85ac7b102
Matthew Gibbons <mhgibbons@me.com>
2024-08-31 06:35:23 +0000

    Don't render MathML by default

--- src/lib/components/Math.svelte
+++ src/lib/components/Math.svelte
@@ -1,17 +1,7 @@
 <script lang="ts">
-	import katex from 'katex';
-
 	const { node }: { node: import('mdast-util-math').Math } = $props();
 
 	const { data, meta, value } = $derived(node);
-
-	const mathml = $derived.by(() =>
-		katex.renderToString(value, {
-			throwOnError: false,
-			displayMode: true,
-			output: 'mathml'
-		})
-	);
 </script>
 
-<pre><code class="language-math math-display" data-meta={meta}>{@html mathml}</code></pre>
+<code class="language-math math-display" data-meta={meta}>{value}</code>
