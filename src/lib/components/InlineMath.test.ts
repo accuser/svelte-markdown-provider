2f522a62daaefe599477cae2d2c7f6680685c432
Matthew Gibbons <mhgibbons@me.com>
2024-08-31 05:53:44 +0000

    Tests

---
+++ src/lib/components/InlineMath.test.ts
@@ -0,0 +1,57 @@
+import { render } from '@testing-library/svelte';
+import { describe, expect, it } from 'vitest';
+import InlineMath from './InlineMath.svelte';
+
+describe('InlineMath.svelte', async () => {
+	it('renders <code>', async () => {
+		const { container } = render(InlineMath, {
+			props: {
+				node: {
+					type: 'inlineMath',
+					value: 'C'
+				}
+			}
+		});
+
+		expect(container.querySelector('code')).toBeInTheDocument();
+	});
+
+	it('renders <code> with `language-math` class', async () => {
+		const { container } = render(InlineMath, {
+			props: {
+				node: {
+					type: 'inlineMath',
+					value: 'C'
+				}
+			}
+		});
+
+		expect(container.querySelector('code.language-math')).toBeInTheDocument();
+	});
+
+	it('renders <code> with `math-inline` class', async () => {
+		const { container } = render(InlineMath, {
+			props: {
+				node: {
+					type: 'inlineMath',
+					value: 'C'
+				}
+			}
+		});
+
+		expect(container.querySelector('code.math-inline')).toBeInTheDocument();
+	});
+
+	it('renders <code> with content', async () => {
+		const { container } = render(InlineMath, {
+			props: {
+				node: {
+					type: 'inlineMath',
+					value: 'C'
+				}
+			}
+		});
+
+		expect(container.querySelector('code')).toHaveTextContent('C');
+	});
+});
---
+++ src/lib/components/Math.test.ts
@@ -0,0 +1,57 @@
+import { render } from '@testing-library/svelte';
+import { describe, expect, it } from 'vitest';
+import Math from './Math.svelte';
+
+describe('Math.svelte', async () => {
+	it('renders <code>', async () => {
+		const { container } = render(Math, {
+			props: {
+				node: {
+					type: 'math',
+					value: 'C'
+				}
+			}
+		});
+
+		expect(container.querySelector('code')).toBeInTheDocument();
+	});
+
+	it('renders <code> with `language-math` class', async () => {
+		const { container } = render(Math, {
+			props: {
+				node: {
+					type: 'math',
+					value: 'C'
+				}
+			}
+		});
+
+		expect(container.querySelector('code.language-math')).toBeInTheDocument();
+	});
+
+	it('renders <code> with `math-display` class', async () => {
+		const { container } = render(Math, {
+			props: {
+				node: {
+					type: 'math',
+					value: 'C'
+				}
+			}
+		});
+
+		expect(container.querySelector('code.math-display')).toBeInTheDocument();
+	});
+
+	it('renders <code> with content', async () => {
+		const { container } = render(Math, {
+			props: {
+				node: {
+					type: 'math',
+					value: 'C'
+				}
+			}
+		});
+
+		expect(container.querySelector('code')).toHaveTextContent('C');
+	});
+});
