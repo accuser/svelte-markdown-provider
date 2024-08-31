5abf7154698360bd7f49b7df9bce255f7b3f5cc5
Matthew Gibbons <mhgibbons@me.com>
2024-08-31 06:11:21 +0000

    Don't style code blocks

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,15 +15,6 @@ export default {
                             'code::after': {
                                 content: 'none',
                             },
-                            code: {
-                                color: 'var(--tw-prose-pre-bg)',
-                                backgroundColor: 'var(--tw-prose-pre-code)',
-                                borderRadius: theme('borderRadius.DEFAULT'),
-                                paddingLeft: theme('spacing.1'),
-                                paddingRight: theme('spacing.1'),
-                                paddingTop: theme('spacing[0.5]'),
-                                paddingBottom: theme('spacing[0.5]'),
-                            },
                         },
                     }
                 }
