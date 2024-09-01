b10ad49c973517bbfdd665fee3be5a308ba4d097
Matthew Gibbons <mhgibbons@me.com>
2024-09-01 09:56:42 +0000

    Unwrap single paragraph in list item

--- src/lib/components/ListItem.svelte
+++ src/lib/components/ListItem.svelte
@@ -3,7 +3,14 @@
 
 	const { node }: { node: import('mdast').ListItem } = $props();
 
-	const { checked, children, data, spread } = $derived(node);
+	const { checked, children, data, spread } = $derived.by(() => ({
+		...node,
+		children:
+			// If the list item contains a single paragraph, unwrap it
+			node.children && node.children.length === 1 && node.children[0].type === 'paragraph'
+				? node.children[0].children
+				: node.children
+	}));
 </script>
 
 <li>
