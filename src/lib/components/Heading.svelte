7ca7421427255922c1c2b611eedb994ab5c851e0
Matthew Gibbons <mhgibbons@me.com>
2024-09-01 10:33:52 +0000

    Use provided slugify or default

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import defaultSlugify from '$lib/defaults/slugify.js';
+	import MARKDOWN_SLUGIFY_TOKEN from '$lib/tokens/markdown-slugify.token.js';
 	import { toString } from 'mdast-util-to-string';
+	import { getContext, hasContext } from 'svelte';
 	import Node from './Node.svelte';
 
 	const { node }: { node: import('mdast').Heading } = $props();
@@ -8,13 +11,11 @@
 
 	let tag = $derived.by(() => `h${depth}`);
 
-	const id = $derived.by(() =>
-		toString(children)
-			.toLowerCase()
-			.replace(/[^\w]+/g, '-')
-			.replace(/-+/, '-')
-			.replace(/^-|-$/g, '')
-	);
+	const slugify = hasContext(MARKDOWN_SLUGIFY_TOKEN)
+		? getContext<(str: string) => string>(MARKDOWN_SLUGIFY_TOKEN)
+		: defaultSlugify;
+
+	const id = $derived.by(() => slugify(toString(children)));
 </script>
 
 <svelte:element this={tag} {id}
