bdfbe6804c1be9331b4abbae9ec2d77473ea5e9c
Matthew Gibbons <mhgibbons@me.com>
2024-09-01 09:57:27 +0000

    Unwrap single paragraph with single image

--- src/lib/components/Paragraph.svelte
+++ src/lib/components/Paragraph.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
+	import Image from './Image.svelte';
 	import Node from './Node.svelte';
 
 	const { node }: { node: import('mdast').Paragraph } = $props();
-
-	const { children, data } = $derived(node);
 </script>
 
-<p>
-	{#each children as node}<Node {node} />{/each}
-</p>
+{#if node.children && node.children.length > 1 && node.children[0].type === 'image'}
+	<Image node={node.children[0]} />
+{:else}
+	{@const { children } = node}
+	<p>
+		{#each children as node}<Node {node} />{/each}
+	</p>
+{/if}
