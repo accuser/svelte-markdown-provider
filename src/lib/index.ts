1c9e3cdad913d3a6b639dcecccaf7a729e2fe8bc
Matthew Gibbons <mhgibbons@me.com>
2024-09-02 04:43:25 +0000

    Don't export `astFromString`

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,2 @@
 export { default as Markdown } from './components/Markdown.svelte';
 export { default as Node } from './components/Node.svelte';
-export { default as astFromString } from './defaults/ast-from-string.js';
