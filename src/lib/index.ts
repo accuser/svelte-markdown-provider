bfc02a642ce8491737086dc5d0a01b6765f779c3
Matthew Gibbons <mhgibbons@me.com>
2024-09-02 04:42:28 +0000

    Export `Node`, but not defaults

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,5 @@
 export { default as Markdown } from './components/Markdown.svelte';
-export {
-	DEFAULT_EXTENSIONS,
-	DEFAULT_MDAST_EXTENSIONS,
-	default as astFromString
-} from './defaults/ast-from-string.js';
+export { default as Node } from './components/Node.svelte';
+export { default as astFromString } from './defaults/ast-from-string.js';
 export type { Components } from './types/components.js';
 export type { Directives } from './types/directives.js';
