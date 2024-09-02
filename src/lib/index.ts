634b53d2b654ecf981eb57dc6b7fffa78bf7bffe
Matthew Gibbons <mhgibbons@me.com>
2024-09-02 04:42:53 +0000

    Don't export types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,3 @@
 export { default as Markdown } from './components/Markdown.svelte';
 export { default as Node } from './components/Node.svelte';
 export { default as astFromString } from './defaults/ast-from-string.js';
-export type { Components } from './types/components.js';
-export type { Directives } from './types/directives.js';
