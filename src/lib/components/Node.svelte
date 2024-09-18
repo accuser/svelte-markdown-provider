63b9186e9ca5c36e605d1dd9c5d2f9d3eb5e163c
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-18 04:59:01 +0000

    Simple derivation using type guards

--- src/lib/components/Node.svelte
+++ src/lib/components/Node.svelte
@@ -3,24 +3,22 @@
 	import MARKDOWN_COMPONENTS_TOKEN from '$lib/tokens/markdown-components.token.js';
 	import MARKDOWN_DIRECTIVES_TOKEN from '$lib/tokens/markdown-directives.token.js';
 	import isDirective from '$lib/type-guards/is-directive.js';
+	import isNode from '$lib/type-guards/is-node.js';
 	import type { Components } from '$lib/types/components.js';
 	import type { Directives } from '$lib/types/directives.js';
 	import { getContext } from 'svelte';
 
-	const node: import('mdast').Node = $props();
+	const node: import('mdast').Nodes = $props();
 
 	const components = getContext<Components>(MARKDOWN_COMPONENTS_TOKEN);
 	const directives = getContext<Directives>(MARKDOWN_DIRECTIVES_TOKEN);
 
-	const Component = $derived.by((() => {
-		if (isDirective(node) && directives && node.type in directives) {
-			return directives[node.type][node.name];
-		} else if (components && node.type in components) {
-			return components[node.type as keyof Components];
-		} else {
-			return defaultComponents[node.type as keyof Components];
-		}
-	}) as () => import('svelte').Component<import('mdast').Node>);
+	const Component = $derived.by((() =>
+		isDirective(node) && directives && node.type in directives && node.name in directives[node.type]
+			? directives[node.type][node.name]
+			: isNode(node) && components && node.type in components
+				? components[node.type]
+				: defaultComponents[node.type]) as () => import('svelte').Component<import('mdast').Nodes>);
 </script>
 
 <Component {...node} />
