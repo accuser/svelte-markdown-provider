8e0236b1722e390bc92c80fda7589ec6a8ea5557
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-19 06:07:38 +0000

    Added bindable `frontmatter` prop

--- src/lib/components/Markdown.svelte
+++ src/lib/components/Markdown.svelte
@@ -1,38 +1,55 @@
-<script lang="ts">
+<script lang="ts" module>
 	import defaultAstFromString from '$lib/defaults/ast-from-string.js';
 	import defaultSlugify from '$lib/defaults/slugify.js';
-	import MARKDOWN_CONTEXT_TOKEN from '$lib/tokens/markdown-context-token.js';
 	import type { Components } from '$lib/types/components.js';
 	import type { Directives } from '$lib/types/directives.js';
-	import type { MarkdownContext } from '$lib/types/markdown-context.js';
-	import { definitions } from 'mdast-util-definitions';
-	import { toc } from 'mdast-util-toc';
-	import { setContext } from 'svelte';
-	import Node from './Node.svelte';
 
-	type Props = (
+	export type Props = (
 		| { ast: import('mdast').Root; astFromString?: never; src?: never }
 		| { ast?: import('mdast').Root; astFromString?: typeof defaultAstFromString; src: string }
 	) & {
 		components?: Partial<Components>;
 		directives?: Partial<Directives>;
+		frontmatter?: Record<string, unknown>;
 		slugify?: typeof defaultSlugify;
 	};
+</script>
 
-	const {
+<script lang="ts">
+	import setMarkdownContext from '$lib/contexts/set-markdown-context.js';
+	import { definitions } from 'mdast-util-definitions';
+	import { toc } from 'mdast-util-toc';
+	import { parse } from 'yaml';
+	import Node from './Node.svelte';
+
+	let {
+		ast = $bindable(),
 		astFromString = defaultAstFromString,
-		src,
-		ast = $bindable(src ? astFromString(src) : undefined),
 		components,
 		directives,
-		slugify = defaultSlugify
+		frontmatter = $bindable(),
+		slugify = defaultSlugify,
+		src
 	}: Props = $props();
 
-	setContext<MarkdownContext>(MARKDOWN_CONTEXT_TOKEN, {
+	if (src) {
+		ast = astFromString(src);
+	}
+
+	if (frontmatter) {
+		// do nothing
+	} else if (ast && ast.children && ast.children[0] && ast.children[0].type === 'yaml') {
+		frontmatter = parse(ast.children[0].value);
+	} else {
+		frontmatter = {};
+	}
+
+	setMarkdownContext({
 		components,
 		directives,
-		getDefinition: definitions(ast),
-		getToc: (options) => toc(ast, options),
+		frontmatter,
+		getDefinition: definitions(ast as import('mdast').Root),
+		getToc: (options) => toc(ast as import('mdast').Nodes, options),
 		slugify
 	});
 </script>
