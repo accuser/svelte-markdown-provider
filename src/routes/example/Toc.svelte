ecf20677a8d70ffd960b0fe22d16a580c4316310
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-19 06:10:13 +0000

    Use `markdownContext`

--- src/routes/example/Toc.svelte
+++ src/routes/example/Toc.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	import Node from '$lib/components/Node.svelte';
-	import MARKDOWN_CONTEXT_TOKEN from '$lib/tokens/markdown-context-token.js';
-	import type { MarkdownContext } from '$lib/types/markdown-context.js';
+	import markdownContext from '$lib/contexts/markdown-context.js';
 	import { toString } from 'mdast-util-to-string';
-	import { getContext } from 'svelte';
 
 	const { children }: import('mdast-util-directive').LeafDirective = $props();
 
 	const { contents, label = 'Contents' } = $derived.by(() => {
-		const { getToc } = getContext<MarkdownContext>(MARKDOWN_CONTEXT_TOKEN);
+		const { getToc } = markdownContext();
 
 		return {
 			label: children && children.length ? toString(children) : 'Contents',
