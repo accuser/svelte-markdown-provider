9f687caa1522c167e6c9c34aad9e2ed4c27dbb0d
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-21 06:52:02 +0000

    Added integration tests

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
 	server: { host: '0.0.0.0' },
 	test: {
 		environment: 'jsdom',
-		include: ['src/**/*.test.{js,ts}'],
+		include: ['src/**/*.test.{js,ts}', 'tests/**/*.test.{js,ts}'],
 		setupFiles: ['./vitest-setup.js']
 	}
 });
