62d23e4b8a10519e1db112b896ae10e90a44feea
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-21 06:51:35 +0000

    Removed `theme` parameter

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,24 @@ import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['src/**/*.svelte'],
-    theme: {
-        extend: {
-            typography(theme) {
-                return {
-                    DEFAULT: {
-                        css: {
-                            'code::before': {
-                                content: 'none',
-                            },
-                            'code::after': {
-                                content: 'none',
-                            },
-                        },
-                    }
-                }
-            }
-        },
-    },
-    plugins: [typography()],
+	content: ['src/**/*.svelte'],
+	theme: {
+		extend: {
+			typography() {
+				return {
+					DEFAULT: {
+						css: {
+							'code::before': {
+								content: 'none',
+							},
+							'code::after': {
+								content: 'none',
+							},
+						},
+					}
+				}
+			}
+		},
+	},
+	plugins: [typography()],
 }
