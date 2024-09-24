52a21451e1e4f602ac8df1bab5aff941451f1cc6
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-24 08:36:39 +0000

    Simplify

--- src/lib/defaults/ast-from-string.ts
+++ src/lib/defaults/ast-from-string.ts
@@ -2,8 +2,8 @@ import { fromMarkdown } from 'mdast-util-from-markdown';
 import defaultExtensions from './extensions.js';
 import defaultMdastExtensions from './mdast-extensions.js';
 
-export default (src: { toString: () => string }) =>
-	fromMarkdown(src.toString(), {
+export default (src: string) =>
+	fromMarkdown(src, {
 		extensions: defaultExtensions,
 		mdastExtensions: defaultMdastExtensions
 	});
