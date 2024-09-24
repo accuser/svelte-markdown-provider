3d53e850b31c99adf06756791de863dbfc7f3ff6
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-24 08:35:43 +0000

    Don't generate `ast`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	import Markdown from '$lib/components/Markdown.svelte';
-	import astFromString from '$lib/defaults/ast-from-string.js';
 	import type { PageData } from './$types.js';
 	const { data }: { data: PageData } = $props();
 
-	const ast = $derived.by(() => astFromString(data.src));
+	const { src } = $derived(data);
 </script>
 
 <article class="prose prose-lg max-w-prose mx-auto prose-slate dark:prose-invert">
-	<Markdown {ast} />
+	<Markdown {src} />
 </article>
