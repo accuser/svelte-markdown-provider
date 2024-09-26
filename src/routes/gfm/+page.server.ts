33ba7f83bde279e5e92c29a5c12ebc36e6a21ae7
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-26 05:17:03 +0000

    Extract gfm to separate library

--- src/routes/gfm/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs';
-import type { PageServerLoad } from './$types.js';
-
-export const load = (async () => {
-	const src = fs.readFileSync('content/gfm.md').toString('utf-8');
-
-	return { src };
-}) satisfies PageServerLoad;
