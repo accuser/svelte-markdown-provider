cd25fdc1564328fb5ccb6a645753a1cec3bab652
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-26 05:15:00 +0000

    Helper functions

---
+++ src/lib/builders/definition-builder.ts
@@ -0,0 +1,14 @@
+import { collect } from '$lib/defaults/collect.js';
+import { isDefinition } from '@accuser/mdast-util-type-guards';
+
+const definitionsFrom = (root: import('mdast').Root) => collect(root, isDefinition);
+
+export default (root: import('mdast').Root) => {
+	const cache = definitionsFrom(root).reduce(
+		(acc, def) => Object.assign(acc, { [def.identifier]: def }),
+		{} as Record<string, import('mdast').Definition>
+	);
+
+	return (identifier: string | null | undefined) =>
+		identifier !== null && identifier !== undefined ? cache[identifier] : undefined;
+};
