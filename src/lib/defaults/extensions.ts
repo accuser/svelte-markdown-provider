5f5e0acf40ab189a0872ad12c04b78ee9b708358
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-26 05:20:54 +0000

    Common-ish only

--- src/lib/defaults/extensions.ts
+++ src/lib/defaults/extensions.ts
@@ -1,6 +1,4 @@
 import { directive } from 'micromark-extension-directive';
 import { frontmatter } from 'micromark-extension-frontmatter';
-import { gfm } from 'micromark-extension-gfm';
-import { math } from 'micromark-extension-math';
 
-export default [directive(), frontmatter(['yaml']), gfm(), math()];
+export default [directive(), frontmatter(['yaml'])];
--- src/lib/defaults/mdast-extensions.ts
+++ src/lib/defaults/mdast-extensions.ts
@@ -1,11 +1,4 @@
 import { directiveFromMarkdown } from 'mdast-util-directive';
 import { frontmatterFromMarkdown } from 'mdast-util-frontmatter';
-import { gfmFromMarkdown } from 'mdast-util-gfm';
-import { mathFromMarkdown } from 'mdast-util-math';
 
-export default [
-	directiveFromMarkdown(),
-	frontmatterFromMarkdown(['yaml']),
-	gfmFromMarkdown(),
-	mathFromMarkdown()
-];
+export default [directiveFromMarkdown(), frontmatterFromMarkdown(['yaml'])];
