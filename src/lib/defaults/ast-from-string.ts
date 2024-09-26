008ba372b88d68cfed50b94a993544e974c1b55c
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-26 05:19:50 +0000

    Merge options extensions

--- src/lib/defaults/ast-from-string.ts
+++ src/lib/defaults/ast-from-string.ts
@@ -1,9 +1,10 @@
-import { fromMarkdown } from 'mdast-util-from-markdown';
+import type { Root } from 'mdast';
+import { fromMarkdown, type Options } from 'mdast-util-from-markdown';
 import defaultExtensions from './extensions.js';
 import defaultMdastExtensions from './mdast-extensions.js';
 
-export default (src: string) =>
+export default ((src, { extensions, mdastExtensions } = {}) =>
 	fromMarkdown(src, {
-		extensions: defaultExtensions,
-		mdastExtensions: defaultMdastExtensions
-	});
+		extensions: [...defaultExtensions, ...(extensions ?? [])],
+		mdastExtensions: [...defaultMdastExtensions, ...(mdastExtensions ?? [])]
+	})) satisfies (src: string, options?: Options) => Root;
