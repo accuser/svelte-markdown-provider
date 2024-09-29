5d97fda029b673c95b8b73b91014e5d3d77469e6
Matthew Gibbons <matthew@southwestkoala.co.uk>
2024-09-29 05:43:02 +0000

    `ast` is once again bindable

--- src/lib/components/Markdown.svelte
+++ src/lib/components/Markdown.svelte
@@ -1,39 +1,36 @@
-<script lang="ts" module>
-	import type { Components } from '$lib/types/components.js';
-	import type { Directives } from '$lib/types/directives.js';
-	import type { Root } from 'mdast';
-	import type { Options } from 'mdast-util-from-markdown';
-
-	type Props = ({ ast: Root; src?: never } | { ast?: never; src: string }) & {
-		components?: Partial<Components>;
-		directives?: Partial<Directives>;
-		options?: Options;
-	};
-</script>
-
 <script lang="ts">
 	import astBuilder from '$lib/builders/ast-builder.js';
 	import definitionBuilder from '$lib/builders/definition-builder.js';
 	import frontmatterBuilder from '$lib/builders/frontmatter-builder.js';
 	import { setMarkdownContext } from '$lib/contexts/markdown-context.js';
 	import astFromString from '$lib/defaults/ast-from-string.js';
+	import type { Components } from '$lib/types/components.js';
+	import type { Directives } from '$lib/types/directives.js';
+	import type { Root } from 'mdast';
+	import type { Options } from 'mdast-util-from-markdown';
 	import Node from './Node.svelte';
 
-	let { ast, components, directives, options, src }: Props = $props();
+	let {
+		ast = $bindable(),
+		components,
+		directives,
+		options,
+		src
+	}: ({ ast: Root; src?: never } | { ast?: Root; src: string }) & {
+		components?: Partial<Components>;
+		directives?: Partial<Directives>;
+		options?: Options;
+	} = $props();
 
-	let mdast = $derived.by(() => {
+	$effect(() => {
 		if (src) {
-			return astFromString(src, options);
-		} else if (ast) {
-			return ast;
+			ast = astFromString(src, options);
 		}
-
-		throw new Error('Either `ast` or `src` must be provided');
 	});
 
-	let getAst = $derived.by(() => astBuilder(mdast));
-	let getDefinition = $derived.by(() => definitionBuilder(mdast));
-	let getFrontmatter = $derived.by(() => frontmatterBuilder(mdast));
+	let getAst = $derived.by(() => astBuilder(ast));
+	let getDefinition = $derived.by(() => definitionBuilder(ast));
+	let getFrontmatter = $derived.by(() => frontmatterBuilder(ast));
 
 	setMarkdownContext({
 		components,
@@ -44,4 +41,6 @@
 	});
 </script>
 
-<Node {...mdast} />
+{#if ast}
+	<Node {...ast} />
+{/if}
